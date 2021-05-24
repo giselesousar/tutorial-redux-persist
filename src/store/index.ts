48843cae846fd1a5d0929290725ab8c480dc2462
Gisele Ribeiro <giselesousar@gmail.com>
2021-05-24 11:05:32 -0300

    Update index.ts

--- src/store/index.ts
+++ src/store/index.ts
@@ -3,7 +3,7 @@
 import { createStore } from 'redux';
 import rootReducer from './reducers/rootReducer';
 import { persistStore, persistReducer} from 'redux-persist';
-import AsyncStorage from '@react-native-community/async-storage';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const persistConfig = {
   key: 'root',
